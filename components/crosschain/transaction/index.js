e76dd378e652eb983939847e9316d00f2902f755
Sirapop <sirapop@Sirapops-MacBook-Air.local>
2021-12-23 12:56:07 +0700

    use amount

--- components/crosschain/transaction/index.js
+++ components/crosschain/transaction/index.js
@@ -297,20 +297,19 @@ export default function Transaction({ data, className = '' }) {
                           </div>
                         )}
                       </div>
-                      {false && (
-                        <div className="flex flex-col sm:flex-row sm:items-center sm:space-x-1 xl:space-x-2">
-                          <div className="flex items-center text-gray-400 dark:text-gray-500">
-                            Fees
-                            <span className="hidden sm:block">:</span>
-                          </div>
-                          {receiver?.value && sender?.value && (
-                            <div className="max-w-min bg-gray-100 dark:bg-gray-800 rounded text-sm space-x-1 py-1 px-2">
-                              <span className="font-semibold">{currency_symbol}{numberFormat(sender.value - receiver.value, '0,0.00000000')}</span>
-                              {/*<span className="uppercase text-gray-600 dark:text-gray-400">{receiver.receivingAsset?.contract_ticker_symbol || receiver.sendingAsset?.contract_ticker_symbol}</span>*/}
-                            </div>
-                          )}
+                      <div className="flex flex-col sm:flex-row sm:items-center sm:space-x-1 xl:space-x-2">
+                        <div className="flex items-center text-gray-400 dark:text-gray-500">
+                          Total Fees Paid
+                          <span className="hidden sm:block">:</span>
                         </div>
-                      )}
+                        {/*receiver?.value && sender?.value*/receiver?.normalize_amount && sender?.normalize_amount && (
+                          <div className="max-w-min bg-gray-100 dark:bg-gray-800 rounded text-sm space-x-1 py-1 px-2">
+                            {/*<span className="font-semibold">{currency_symbol}{numberFormat(sender.value - receiver.value, '0,0.00000000', true)}</span>*/}
+                            <span className="font-semibold">{numberFormat(sender.normalize_amount - receiver.normalize_amount, '0,0.00000000', true)}</span>
+                            <span className="uppercase text-gray-600 dark:text-gray-400">{receiver.receivingAsset?.contract_ticker_symbol || receiver.sendingAsset?.contract_ticker_symbol}</span>
+                          </div>
+                        )}
+                      </div>
                     </>
                   )}
                   <div className="flex items-center space-x-2 mx-auto py-2">
@@ -431,20 +430,19 @@ export default function Transaction({ data, className = '' }) {
                         </div>
                       )}
                     </div>
-                    {false && (
-                      <div className="flex flex-col sm:flex-row sm:items-center sm:space-x-1 xl:space-x-2">
-                        <div className="flex items-center text-gray-400 dark:text-gray-500">
-                          Fees
-                          <span className="hidden sm:block">:</span>
-                        </div>
-                        {receiver?.value && sender?.value && (
-                          <div className="max-w-min bg-gray-100 dark:bg-gray-800 rounded text-sm space-x-1 py-1 px-2">
-                            <span className="font-semibold">{currency_symbol}{numberFormat(sender.value - receiver.value, '0,0.00000000', true)}</span>
-                            {/*<span className="uppercase text-gray-600 dark:text-gray-400">{receiver.receivingAsset?.contract_ticker_symbol || receiver.sendingAsset?.contract_ticker_symbol}</span>*/}
-                          </div>
-                        )}
+                    <div className="flex flex-col sm:flex-row sm:items-center sm:space-x-1 xl:space-x-2">
+                      <div className="flex items-center text-gray-400 dark:text-gray-500">
+                        Total Fees Paid
+                        <span className="hidden sm:block">:</span>
                       </div>
-                    )}
+                      {/*receiver?.value && sender?.value*/receiver?.normalize_amount && sender?.normalize_amount && (
+                        <div className="max-w-min bg-gray-100 dark:bg-gray-800 rounded text-sm space-x-1 py-1 px-2">
+                          {/*<span className="font-semibold">{currency_symbol}{numberFormat(sender.value - receiver.value, '0,0.00000000', true)}</span>*/}
+                          <span className="font-semibold">{numberFormat(sender.normalize_amount - receiver.normalize_amount, '0,0.00000000', true)}</span>
+                          <span className="uppercase text-gray-600 dark:text-gray-400">{receiver.receivingAsset?.contract_ticker_symbol || receiver.sendingAsset?.contract_ticker_symbol}</span>
+                        </div>
+                      )}
+                    </div>
                     <div className="flex items-center space-x-2 mx-auto py-2">
                       {data ?
                         general?.sendingChain && (
