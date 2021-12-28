826bcfffed98a7495499795f5a62dc0f85da0e29
Sirapop <sirapop@Sirapops-MacBook-Air.local>
2021-12-28 09:28:38 +0700

    trim

--- components/navbar/search/index.js
+++ components/navbar/search/index.js
@@ -58,7 +58,7 @@ export default function Search() {
           <input
             ref={inputSearchRef}
             value={inputSearch}
-            onChange={event => setInputSearch(event.target.value)}
+            onChange={event => setInputSearch(event.target.value?.trim())}
             type="search"
             placeholder="Search by Router / Address / Tx ID"
             className="w-60 sm:w-72 xl:w-96 h-8 sm:h-10 appearance-none rounded text-xs pl-2 sm:pl-8 pr-0 sm:pr-3 focus:outline-none"
