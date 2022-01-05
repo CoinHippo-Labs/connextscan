a27ef7f3234f48a7921055031911959dd4439222
Sirapop <sirapop@Sirapops-MacBook-Air.local>
2022-01-05 18:16:36 +0700

    skipPolling

--- components/chain-meta/index.js
+++ components/chain-meta/index.js
@@ -320,7 +320,7 @@ export default function ChainMeta() {
           }
         }
 
-        setSdk(await NxtpSdk.create({ chainConfig, signer: Wallet.createRandom() }))
+        setSdk(await NxtpSdk.create({ chainConfig, signer: Wallet.createRandom(), skipPolling: true }))
       }
       else {
         if (routers_status_refresh) {
--- components/crosschain/transaction/index.js
+++ components/crosschain/transaction/index.js
@@ -117,7 +117,7 @@ export default function Transaction({ data, className = '' }) {
         }
       }
 
-      const sdk = await NxtpSdk.create({ chainConfig, signer })
+      const sdk = await NxtpSdk.create({ chainConfig, signer, skipPolling: true })
 
       let response
 
