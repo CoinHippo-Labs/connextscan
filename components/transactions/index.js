fef8959f2025837bf599685231dad25a991e9ebb
Sirapop <sirapop@Sirapops-MacBook-Air.local>
2022-03-10 00:59:23 +0700

    update dep

--- components/transactions/index.js
+++ components/transactions/index.js
@@ -93,7 +93,7 @@ export default function Transactions({ addTokenToMetaMaskFunction, className = '
   useEffect(() => {
     const controller = new AbortController()
 
-    const getTxs = async (chain, q) => {
+    const getTxs = async chain => {
       if (chain && !chain.disabled) {
         if (!controller.signal.aborted) {
           let params
@@ -114,25 +114,6 @@ export default function Transactions({ addTokenToMetaMaskFunction, className = '
               type: TRANSACTIONS_DATA,
               value: { [`${chain.chain_id}`]: response?.data || [] },
             })
-
-            // const routers = _.reverse(['0x6db8506a7454c5a83b9e68dfc89fd7413ce97a5d', '0x95ce8b1c273af612cd895e6b0c633039c3572827', '0x1f6853bc0cc4dccd2ebf7b36c3b34dac0c1b1096', '0x31769170acae5f8c06a4577e7ff8719d742fd4c5', '0x9f27a9cacef594eb60dc4e8e6cc1f5817fd7b1af', '0x92495600b72ef0e1fa22453b58938a9af49918ae', '0xd7d8e48abdcbd61add5d533d7ce542e54a5c3975', '0xe439ca609b964ab9422672bf83b8e171e90adad1', '0x82f6a96481433eef26bb506887eb064ccebd3b81', '0xd19b8a5be17d68f74789a9f87d20cf924f491613', '0x55af16ee16b5002d5d8baa45d59e14515db47acd', '0x8640a7769ba59e219d85802427a964068d4d99f8', '0x75ee04316185200a51e995779f381ef63d39f7f9', '0x8c336154121c0d70133b8c7f906a729cf034e5be', '0x7e76de758cd414096a4882ed2824bc513d7ed7c9', '0xa9bfe31527e8e0845cf345549f68828e6775f8c0', '0x7581f456cf77ce416c2a4a4a43857a38cbe0bafe', '0xce89263c451d62caefc54811e86e8f930204223d', '0xe4747a26b941ebdbc7ef67de7580f700e3dbf6d5', '0xee2ef40f688607cb23618d9312d62392786d13eb', '0xcf632daf0c48b075864e54a625bb655d63a3ce50', '0x724346027f0de92a3b2c8f18956103f76cda3dd4', '0x2452b99b8beca1453262fee43d2181b053291549', '0x56723283704c7f8ea3e13650413af102d4572e48', '0x2a3248456464a19fa913bd5dfa952999365b31ce', '0x467517104f300417e078d003b8817531dece4ad3', '0x84979b230fa868c1dc8e78b72fd8f5438fba032b', '0xe026086181bcfbd06db4c67739aa9c36054d5551', '0xf410f49c547115567f4209bae3dd2dbd855ddac0', '0x98a429d65e96fd048867c31e2da74573e88f2fb3', '0x83baadc390a6c63df8b3486425758d62a8bc141a', '0x3df415d9e0539de5e746ff36ff98b54cf6570722', '0x1eab42ce26873e6365bd3fffea23e59591f660e0', '0xa5b725e6e87fcd5eede4a6b7c5a47e4090f49239', '0xb3ac2b73fa658ee49abf8bc691c1239557c5cc6d', '0x1370c680f2a2ae22f711b30b917a57f052816485', '0x6fd84ba95525c4ccd218f2f16f646a08b4b0a598', '0x49b296a849dfeb21184b591b25c9d7755dfcbec5', '0xae1e59402a3f483f0bbb663800ff21a0f2b1701d', '0x6f41ff4425e6051b3b941e3d721d29199480ab58', '0x29a519e21d6a97cdb82270b69c98bac6426cdcf9', '0x22c44b2a5fd493e11090d732a824d334f5be6f21', '0xe6a3dc2971532f5feb6b650e5dda2fe923d13af2'])
-            let total = 0
-            // for (let i = 0; i < routers.length; i++) {
-              if (/*routers[i] && */([56, 137, 250, 100, 42161, 1285].includes(chain.chain_id))) {
-                const max_size = 100, size = 10
-                let x, start = q ? 0 : 0
-                while(!x || x.length === /*max_*/size) {
-                  const res = await getTransactions(sdk_data, chain.chain_id, null, { max_size, direction: q ? 'desc' : 'asc', size, start, sync: true/*, where: `{ router: "${routers[i].toLowerCase()}" }`*/ }, chains_data, tokens_data)
-                  x = res?.data
-                  if (x) {
-                    start+=x.length
-                    total+=x.length
-                  }
-                  // console.log(`${chain.chain_id}: ${routers[i]}[${i}] ${total} ${start} ${x?.length}`)
-                  console.log(`${chain.chain_id}: ${total} ${start} ${x?.length}`)
-                }
-              }
-            // }
           }
         }
       }
@@ -141,16 +122,15 @@ export default function Transactions({ addTokenToMetaMaskFunction, className = '
     const getData = () => {
       if (chains_data && sdk_data) {
         chains_data.forEach(c => getTxs(c))
-        chains_data.forEach(c => getTxs(c, true))
       }
     }
 
     getData()
 
-    // const interval = setInterval(() => getData(), 5 * 60 * 1000)
+    const interval = setInterval(() => getData(), 5 * 60 * 1000)
     return () => {
       controller?.abort()
-      // clearInterval(interval)
+      clearInterval(interval)
     }
   }, [pathname, address, blockchain_id, sdk_data])
 
